7013d03964e39d6d72e259d68c8dce73743055b0
Anton Ryazanov <funfun2010@mail.ru>
2019-02-17 18:18:42 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'bundle.js',
-        publicPath: "https://goldenfishe.github.io/wegl/"
+        publicPath: "/wegl/"
     },
     module: {
         rules: [
